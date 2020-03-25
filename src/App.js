7f92355e59c19df940ea834f0f817f49cbcb6535
djrcoder <djrcode@gmail.com>
2020-03-25 20:46:38 +0900

    Add csv.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const csv = require("csvtojson");
+
 function App() {
+
+  const converter = csv()
+    .fromFile("../data/one-thousand-ingredients.csv")
+    .then(json => console.log(json))
+
+  converter();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <h3>HELLO</h3>
+
+
+
+
+
     </div>
   );
 }
