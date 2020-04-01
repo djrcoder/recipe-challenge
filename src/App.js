a64cf18ecdb98e5b1a910ed18a078274aaa2ce32
djrcoder <djrcode@gmail.com>
2020-04-01 21:16:44 +0900

    Removed csv parse.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-var customData = require('./data/ingredients.json');
+
+const customData = require('./data/ingredients.json');
+const ingredientArray = [];
+
+// Define limits for randomNum function
+const minimum = 0;
+const maximum = 1000;
+
+// set limit for number of results
+const recipeSize = 8;
+
+function randomNum(minimum, maximum) {
+  const rand = Math.floor(Math.random() * (maximum - minimum + 1) + minimum)
+  return rand;
+}
 
 
 function App() {
 
+  for (let i = 0; i < recipeSize; i++) {
+    ingredientArray.push(customData[randomNum(minimum, maximum)].ingredient)
+  }
 
-  console.log(customData)
+  console.log(ingredientArray)
 
   return (
     <div className="App">
@@ -14,10 +31,13 @@ function App() {
       </header>
 
       <h3>HELLO</h3>
+      <p>
+      </p>
 
 
-
-
+      {ingredientArray.map(food => (
+        <div className="Ingredients" key={food}>{food}</div>
+      ))}
 
     </div>
   );
